d81252b9aebd8c029c5afac111fdbc6f9d153c49
Adarsh Bhardwaj <adarshbhardwaj1919@gmail.com>
2025-09-10 11:05:15 +0530

    Fix nodemon crash and database connection

--- src/db/index.js
+++ src/db/index.js
@@ -1,34 +1,14 @@
-// import mongoose from "mongoose";
-// import { DB_NAME } from "../constants.js";
-
-// const connectDB = async () => {
-//     try {
-//        const connectionInstance = await mongoose.connect(`{process.env.MONGODB_URI}/${DB_NAME}`)
-//        console.log(`\n MongoDB connected !! DB HOST: $
-//         {connectionInstance.connection.host}`)
-//     } catch (error) {
-//         console.log("MONGODB connection error", error);
-//         process.exit(1)
-//     }
-// }
-// export default connectDB
-
 import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
-  try {
-    const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${process.env.DB_NAME}?retryWrites=true&w=majority`,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
-    console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
-  } catch (error) {
-    console.log("MONGODB connection error", error);
-    process.exit(1);
-  }
-};
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`)
+    } catch (error) {
+        console.log("MONGODB connection error", error);
+        process.exit(1)
+    }
+}
 
-export default connectDB;
+export default connectDB
